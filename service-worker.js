1290dc570931ae28e2e2a5edb74cbfc08d898542
Ben <72495589+bworth@users.noreply.github.com>
2020-10-16 12:49:35 +0100

    Change service worker to a cache first offline policy

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,34 @@
 const CACHE_NAME = 'cv-cache-v1';
 const OFFLINE_URLS = ['index.html', './', './?source=pwa'];
 
+function preferCache(request) {
+	return caches.match(request).then((cachedResponse) => {
+		if (cachedResponse) {
+			console.log('[fetch] event serving from cache', request.url);
+			return cachedResponse;
+		}
+
+		console.warn('[fetch] event unable to find cached resource, fetching from network', request.url);
+		return fetch(request).then((response) => {
+			caches.open(CACHE_NAME).then((cache) => cache.put(request, response.clone()));
+			return response;
+		});
+	});
+}
+
+function preferNetwork(request) {
+	return fetch(request).catch((error) => {
+		console.warn('[fetch] event failed; serving cached fallback.', error);
+		return caches.open(CACHE_NAME).then((cache) => cache.match(request).then((matching) => {
+			if (matching) {
+				return matching;
+			}
+
+			console.warn('[fetch] event unable to find cached resource', request.url);
+		}));
+	});
+}
+
 self.addEventListener('install', (event) => {
 	const requests = OFFLINE_URLS.map((url) => new Request(url, { cache: 'reload' }));
 
@@ -8,9 +36,10 @@ self.addEventListener('install', (event) => {
 		caches.open(CACHE_NAME).then((cache) => cache.addAll(requests)
 			.then(() => {
 				const urls = requests.map((request) => request.url);
-				console.log('Install event cached offline resources', urls.join(', '));
+				console.log('[install] event cached resources', urls.join(', '));
+				return self.skipWaiting();
 			})
-			.catch((error) => console.warn('Install event failed.', error))
+			.catch((error) => console.warn('[install] event precache failed.', error))
 		)
 	);
 });
@@ -22,6 +51,10 @@ self.addEventListener('activate', (event) => {
 		caches.keys()
 			.then((cacheNames) => cacheNames.filter((cacheName) => !activeCacheNames.includes(cacheName)))
 			.then((oldCacheNames) => Promise.all(oldCacheNames.map((oldCacheName) => caches.delete(oldCacheName))))
+			.then(() => {
+				console.log('[activate] event claiming control with', activeCacheNames.join(', '));
+				self.clients.claim();
+			})
 	);
 });
 
@@ -29,11 +62,6 @@ self.addEventListener('fetch', (event) => {
 	const request = event.request;
 
 	if (request.method === 'GET') {
-		event.respondWith(
-			fetch(request).catch((error) => {
-				console.warn('Fetch event failed; serving cached offline fallback.', error);
-				return caches.open(CACHE_NAME).then((cache) => cache.match(request));
-			})
-		);
+		event.respondWith(preferCache(request));
 	}
 });
